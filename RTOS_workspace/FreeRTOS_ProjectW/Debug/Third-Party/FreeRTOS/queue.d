d2df03593a2d36fa39a7ba6de9db7f3eede4062c
sumansekhar <sumansekharsahoo10@gmail.com>
2022-05-14 20:41:25 +0530

    FreeRTOS layer successfully added and basic workspace done

---
+++ RTOS_workspace/FreeRTOS_ProjectW/Debug/Third-Party/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+Third-Party/FreeRTOS/queue.o: ../Third-Party/FreeRTOS/queue.c \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/FreeRTOS.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/FreeRTOSConfig.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/projdefs.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/portable.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/deprecated_definitions.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/mpu_wrappers.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/list.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/queue.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/FreeRTOS.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/FreeRTOSConfig.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/projdefs.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/portable.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/deprecated_definitions.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/mpu_wrappers.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/list.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/queue.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h:
---
+++ RTOS_workspace/FreeRTOS_ProjectW/Debug/Third-Party/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+Third-Party/FreeRTOS/stream_buffer.o: \
+ ../Third-Party/FreeRTOS/stream_buffer.c \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/FreeRTOS.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/FreeRTOSConfig.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/projdefs.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/portable.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/deprecated_definitions.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/mpu_wrappers.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/list.h \
+ D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/stream_buffer.h
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/FreeRTOS.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/FreeRTOSConfig.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/projdefs.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/portable.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/deprecated_definitions.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/mpu_wrappers.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/task.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/list.h:
+D:/L_Workspace/RTOS_workspace/FreeRTOS_ProjectW/Third-Party/FreeRTOS/include/stream_buffer.h:
